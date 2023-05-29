0c2b481ecb2dd69e5ff053dc0e9617068a54d1d0
Abdul Rafay <99marafay@gmail.com>
2023-05-30 02:56:32 +0500

    Updating the config Files

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,63 +1,71 @@
-# display neofetch
-# neofetch
-winfetch
+# Import-Module PSReadLine
+neofetch
+# starship Promote
+Invoke-Expression (&starship init powershell)
+# this is the terminal icons for dir command
+Import-Module -Name Terminal-Icons
+Set-PSReadLineOption -PredictionSource History
+                                         
 
-# Find out if the current user identity is elevated (has admin rights)
-$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
-$principal = New-Object Security.Principal.WindowsPrincipal $identity
-$isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-                                             
-# this function will launch nodepad
-function n      { notepad $args }
-
-# This function will prmote the current user to admin
-function prompt 
-{ 
-    if ($isAdmin) 
-    {
-        "[" + (Get-Location) + "] # " 
+#  Open Up the ::Administrator Powershell
+function sudo {
+    if ($args.Count -gt 0) {
+        $argList = "& '" + $args + "'"
+        $wtExe = "wt.exe"
+        Start-Process -FilePath $wtExe -ArgumentList "-p", "Windows PowerShell", "-NoExit", "-c", $argList -Verb RunAs
     }
-    else 
-    {
-        "[" + (Get-Location) + "] $ "
+    else {
+        $wtExe = "wt.exe"
+        Start-Process -FilePath $wtExe -ArgumentList "-p", "Windows PowerShell", "pwsh.exe" -Verb RunAs
     }
 }
 
-$Host.UI.RawUI.WindowTitle = "PowerShell {0}" -f $PSVersionTable.PSVersion.ToString()
-if ($isAdmin)
-{
-    $Host.UI.RawUI.WindowTitle += " [ADMIN]"
+# Function to Remove Items 
+function rm {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Path
+    )
+
+    Remove-Item -Path $Path -Recurse -Force
 }
 
-# Simple function to start a new elevated process. If arguments are supplied then 
-# a single command is started with admin rights; if not then a new admin instance
-# of PowerShell is started.
-function admin
-{
-    if ($args.Count -gt 0)
-    {   
-       $argList = "& '" + $args + "'"
-       Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $argList
-    }
-    else
-    {
-       Start-Process "$psHome\powershell.exe" -Verb runAs
-    }
+# function to create a new file
+function touch {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0, Mandatory = $true)]
+        [Alias("Path")]
+        [string]$FilePath,
+        
+        [Parameter(Position = 1)]
+        [Alias("ItemType")]
+        [string]$Type = "File"
+    )
+
+    New-Item -Path $FilePath -ItemType $Type
 }
 
+# Function that will read file content and thn displayed into the terminal
+function cat {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [ValidateScript({Test-Path $_ -PathType 'Leaf'})]
+        [string]$FilePath
+    )
 
-# Set UNIX-like aliases for the admin command, so sudo <command> will run the command
-# with elevated rights. 
-Set-Alias -Name su -Value admin
-Set-Alias -Name sudo -Value admin
-# Set-Alias -Name cat -Value Get-Content
-# Set-Alias -Name touch -Value New-Item
-
+    $content = Get-Content -Path $FilePath
+    $content
+}
 
 # This function will reload the profile
 function reload-profile {
     & $profile
 }
+
 # this function will find  the file in the current directory
 function find-file($name) {
     Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
@@ -76,29 +84,41 @@ function pkill($name) {
     Get-Process $name -ErrorAction SilentlyContinue | Stop-Process
 }
 
-# starship Promote
-Invoke-Expression (&starship init powershell)
-# this is the terminal icons for dir command
-Import-Module -Name Terminal-Icons
-# Adding the tab collection to the Powershell
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+# Function to restart a system 
+function Restart-System {
+    [CmdletBinding()]
+    param (
+        [switch]$Force
+    )
 
-                                         
-# Alias for Powershell
-Set-Alias -Name Restart-Computer -Value reboot
-Set-Alias -Name ls -Value dir
-Set-Alias -Name Stop-Computer -Value Shutdown
+    if ($Force) {
+        Restart-Computer -Force
+    }
+    else {
+        Restart-Computer
+    }
+}
+# A function for shutting Down the system
+function Shutdown-System {
+    [CmdletBinding()]
+    param (
+        [switch]$Force
+    )
 
-# These are the command that will can give the powershell like fish shell abilities
-Import-Module PSReadLine
-Set-PSReadLineOption -PredictionSource History
+    if ($Force) {
+        Stop-Computer -Force
+    }
+    else {
+        Stop-Computer
+    }
+}
 
-
-# Programs used:
-# All of these Application can be installed using Sccop 
-# learn about scoop here: https://scoop.sh/
-# Sccop 
-# winfetch or Neofetch 
-# terminal-icons
-#  lsd
-# starship
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
